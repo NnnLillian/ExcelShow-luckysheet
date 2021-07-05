1fc840d689887e569176c7dfb9d816240eb5fd44
weijw <weijw@mail.jj.cn>
2021-07-05 20:20:57 +0800

    后端数据获取处理

--- src/store/tableData.js
+++ src/store/tableData.js
@@ -3,10 +3,23 @@ const axios = require('axios');
 exports.getTableData = async function(url){
     console.log("getTabelData");
     try{
-        const response = await axios.get(url)
-        console.log("x",response.data.data);
-        return response;
+        const response = await axios.get(url);
+        console.log(deleteNull(response.data.data));
+        return Promise.resolve(response.data);
     } catch (error){
         console.error(error);
+        return Promise.reject(error)
     }
+}
+
+const deleteNull = (sheets) =>{
+    if(Array.isArray(sheets)){
+        sheets.forEach((iSheet)=>{
+            iSheet.data = iSheet.data.map((row)=>{
+                console.log("row", row);
+                return row.filter(r=>r!==null)
+            });
+        });
+    }
+    return sheets;
 }

8bf94a842e61fab169c532d32d30ec6bb38835be
weijw <weijw@mail.jj.cn>
2021-07-07 15:20:02 +0800

    优化细节

--- src/component/Luckysheet.js
+++ src/component/Luckysheet.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { getTableData } from '../store/tableData';
 import { exportExcel } from '../tools/exportExcel';
+import './Luckysheet.css';
 class Luckysheet extends React.Component {
 
-     options = {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: '数据获取中……',
+            pageState: false
+        }
+    }
+    options = {
         container: "luckysheet",
         title: '工位平面图',
-        lang:'zh',
-        // showtoolbar: false,
+        lang: 'zh',
+        showtoolbar: false,
         showstatisticBar: false,
-        showstatisticBarConfig:{
+        showstatisticBarConfig: {
             zoom: true, // 缩放
         },
-        enableAddRow:false,
-        cellRightClickConfig:{
+        enableAddRow: false,
+        cellRightClickConfig: {
             copy: true, // 复制
             copyAs: false, // 复制为
             paste: false, // 粘贴
             insertRow: false, // 插入行
             insertColumn: false, // 插入列
-            // deleteRow: false, // 删除选中行
-            // deleteColumn: false, // 删除选中列
-            // deleteCell: false, // 删除单元格
+            deleteRow: false, // 删除选中行
+            deleteColumn: false, // 删除选中列
+            deleteCell: false, // 删除单元格
             hideRow: false, // 隐藏选中行和显示选中行
             hideColumn: false, // 隐藏选中列和显示选中列
             rowHeight: false, // 行高
@@ -36,47 +44,51 @@ class Luckysheet extends React.Component {
             data: false, // 数据验证
             cellFormat: false // 设置单元格格式
         },
-        sheetFormulaBar:false,
+        functionButton: '<button id="" class="btn btn-primary" style="padding:3px 4px;font-size: 12px;margin-right: 10px;"><i class="fa fa-download" aria-hidden="true"></i>下载</button> ',
+        myFolderUrl: '#',
+        sheetFormulaBar: false,
         defaultRowHeight: 23, //自定义行高
         defaultColWidth: 100, //自定义列宽
-    }
+    };
 
     luckysheet = window.luckysheet;
 
-    getCell=()=>{
+    getCell = () => {
         console.log(this.luckysheet.getLuckysheetfile())
     }
 
-    exportLuckyExcel=()=>{
+    exportLuckyExcel = () => {
         exportExcel(this.luckysheet.getLuckysheetfile())
     }
 
     componentDidMount() {
-        getTableData('https://395a20ae-a4fd-4128-ad34-981ac18912c9.mock.pstmn.io/getSeatMap').then(res=>{
-            console.log("return", res.data.data)
-            this.options['data'] = res.data.data;
-            this.luckysheet.create(this.options);
-        })
+        const tableData = getTableData('https://395a20ae-a4fd-4128-ad34-981ac18912c9.mock.pstmn.io/getSeatMap')
+        console.log("tabledata", tableData);
+        tableData.then(
+            result => {
+                this.options['data'] = result.data;
+                this.luckysheet.create(this.options);
+                this.setState({
+                    pageState: true
+                })
+            },
+            reason => {
+                this.setState({
+                    show: '数据获取失败！'
+                })
+            })
     }
     render() {
-        console.log("render")
-        const luckyCss = {
-            margin: '0px',
-            padding: '0px',
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            left: '0px',
-            top: '0px'
-        }
-
         return (
             <div>
-                <button onClick={this.getCell} style={{position:'absolute', top:'0px', zIndex:5}}>getCell</button>
-                <button onClick={this.exportLuckyExcel} className='btn btn-primary' style={{position:'absolute', top:'12px', right:'10px', zIndex:5}}><i class="fa fa-download" aria-hidden="true"></i>   Export</button>
-                <div id="luckysheet" style={luckyCss}></div>
+                {!this.state.pageState && <span>{this.state.show}</span>}
+                <div>
+                    {/* <button onClick={this.getCell} style={{position:'absolute', top:'0px', zIndex:5}}>getCell</button> */}
+                    <button onClick={this.exportLuckyExcel} className={`btn btn-primary table-export-${this.state.pageState}`}><i className="fa fa-download" aria-hidden="true"></i>   下载</button>
+                    <div id="luckysheet" className="lucky-css"></div>
+                </div>
             </div>
-            
+
         )
     }
 }
--- src/store/tableData.js
+++ src/store/tableData.js
@@ -1,25 +1,35 @@
 const axios = require('axios');
 
-exports.getTableData = async function(url){
-    console.log("getTabelData");
-    try{
+exports.getTableData = async function (url) {
+    // console.log("getTabelData");
+    try {
         const response = await axios.get(url);
-        console.log(deleteNull(response.data.data));
+        // deleteNull(response.data.data);
+        console.log(response.data.data)
         return Promise.resolve(response.data);
-    } catch (error){
+    } catch (error) {
         console.error(error);
         return Promise.reject(error)
     }
 }
 
-const deleteNull = (sheets) =>{
-    if(Array.isArray(sheets)){
-        sheets.forEach((iSheet)=>{
-            iSheet.data = iSheet.data.map((row)=>{
-                console.log("row", row);
-                return row.filter(r=>r!==null)
-            });
-        });
-    }
-    return sheets;
-}
+// 删除空列
+// const deleteNull = (sheets) =>{
+//     if(Array.isArray(sheets)){
+//         sheets.forEach((iSheet)=>{
+//             iSheet.data = iSheet.data.map((row)=>{
+//                 return row.filter(r=>r!==null)
+//             });
+//         });
+//     }
+//     return sheets;
+// }
+
+// 删除空行
+// const deleteNull = (sheets) =>{
+//     if(Array.isArray(sheets)){
+//         sheets.forEach((iSheet)=>{
+//             iSheet.data = iSheet.data.filter((row)=>row[0]!==null)
+//         })
+//     }
+// }
